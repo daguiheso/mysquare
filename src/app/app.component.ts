00eb01d9f2f0174875a5785d205d5d6508701c87
Daniel Hernandez <daguiheso@gmail.com>
2017-11-03 17:12:18 -0500

    Implemented ngSwitch and ngSwitchCase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,18 +8,18 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Mysquare';
   places: any = [
-    { name: 'Panpaya', active: true },
-    { name: 'Dolking Donuts', active: false },
-    { name: 'Sopas de la Abuela', active: true },
-    { name: 'Subway', active: false },
-    { name: 'Cubano', active: true },
-    { name: 'TG Fridays', active: true },
-    { name: 'El Corral', active: false },
-    { name: 'Mtr Lee', active: true },
-    { name: 'Randys', active: true },
-    { name: 'PPC', active: true },
-    { name: 'kokorico', active: true },
-    { name: 'Hornitos', active: false }
+    { name: 'Panpaya', active: true, closeness: 1, distance: 1 },
+    { name: 'Dolking Donuts', active: false, closeness: 1, distance: 1.8 },
+    { name: 'Sopas de la Abuela', active: true, closeness: 1, distance: 1.5 },
+    { name: 'Subway', active: false, closeness: 2, distance: 5 },
+    { name: 'Cubano', active: true, closeness: 3, distance: 10 },
+    { name: 'TG Fridays', active: true, closeness: 3, distance: 18 },
+    { name: 'El Corral', active: false, closeness: 3, distance: 25 },
+    { name: 'Mtr Lee', active: true, closeness: 3, distance: 35 },
+    { name: 'Randys', active: true, closeness: 3, distance: 18 },
+    { name: 'PPC', active: true, closeness: 3, distance: 23 },
+    { name: 'kokorico', active: true, closeness: 3, distance: 50 },
+    { name: 'Hornitos', active: false, closeness: 1, distance: .4 },
   ];
   lat: number = 4.6294303;
   lng: number = -74.092209;
