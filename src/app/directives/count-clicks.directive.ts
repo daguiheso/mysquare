378de2d7607af9c0283e12c72332a3898029d2e7
daguiheso <daguiheso@gmail.com>
2017-11-04 21:05:48 -0500

    Implemented Host Binders

--- src/app/directives/count-clicks.directive.ts
+++ src/app/directives/count-clicks.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, HostListener } from '@angular/core';
+import { Directive, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   // seleccionando solo anclas tags
@@ -8,9 +8,13 @@ import { Directive, HostListener } from '@angular/core';
 export class CountClicksDirective {
 
   clickN = 0;
+  // recibe com 1 param el atributo del elemento de html a modificar
+  @HostBinding('style.opacity') opacity: number = .1;
+
   // $event captura el evento y el target es el elemento
   @HostListener('click', ['$event.target']) onClick(btn) {
     console.log('a', btn, "Número de Clicks:", this.clickN++);
+    this.opacity += .1;
   }
 
   constructor() { }
